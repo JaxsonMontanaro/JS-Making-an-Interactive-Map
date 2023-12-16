a2c9ccec60036b19bef52bdc0c27fe176eda3646
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-12-15 17:00:13 -0800

    updated map size

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ async function getFoursquare(business) {
 		method: 'GET',
 		headers: {
 		Accept: 'application/json',
-		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
+		Authorization: 'fsq3pNtHAVTnOTzxpHKoaZN50w/qYFYRUXjfy/Wm0eZBVeo='
 		}
 	}
 	let limit = 5
